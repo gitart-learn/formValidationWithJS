03b50b232cdeca01093f40c8fc2ba178ddefac33
Oleksandr <sashakryvoruchko11@gmail.com>
2022-06-18 22:20:56 +0300

    made form1 active

---
+++ src/js/data/post-form1.js
@@ -0,0 +1,87 @@
+export const fields = [
+  {
+    inputName: 'title',
+    type: 'text',
+  },
+
+
+  {
+    inputName: 'content',
+    type: 'text',
+  },
+  {
+    inputName: 'date',
+    type: 'date',
+  },
+
+  {
+    inputName: 'radio',
+    type: 'radio',
+  },
+  {
+    inputName: 'show',
+    type: 'switcher',
+  },
+  {
+    inputName: 'tag',
+    type: 'checkbox',
+  },
+
+]
+
+export const fieldsValidationRules = [
+  {
+    inputName: 'title',
+    rules: [
+      {
+        ruleName: 'required',
+        SpecificErrorMessage: 'i said title is a required field',
+      },
+      {
+        ruleName: 'minLength',
+        value: 3,
+        SpecificErrorMessage: 'length of title  should be at least {value} characters',
+
+      },
+    ],
+  },
+
+  {
+    inputName: 'content',
+    rules: [{
+      ruleName: 'required',
+    },
+    {
+      ruleName: 'minLength',
+      value: 20,
+    }],
+  },
+
+
+  {
+    inputName: 'date',
+    rules: [{
+      ruleName: 'required',
+    }],
+  },
+
+  {
+    inputName: 'radio',
+    rules: [{
+      ruleName: 'required',
+    }],
+  },
+  {
+    inputName: 'show',
+    rules: [{
+      ruleName: 'required',
+    }],
+  },
+  {
+    inputName: 'tag',
+    rules: [{
+      ruleName: 'required',
+    }],
+  },
+
+]
--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,11 @@ import { getDataFromForm } from './formHelpers/getDataFromForm'
 import { validateData } from './validateData'
 import { handleErrors } from './formHelpers/handleErrors'
 import { fields, fieldsValidationRules } from './data/post-form'
+import { fields as fieldsForForm1, fieldsValidationRules as fieldsValidationRulesForForm1 } from './data/post-form1'
 
 const form = document.querySelector('#form')
 
-let afterSubmit = false
+let formSubmitted = false
 
 form.addEventListener('submit', (event) => {
   event.preventDefault()
@@ -17,15 +18,48 @@ form.addEventListener('submit', (event) => {
 
   handleErrors(errors, form)
 
-  afterSubmit = true
+  formSubmitted = true
+
+  if (errors.length === 0) {
+    console.log(data)
+  }
 })
 
 form.addEventListener('input', (event) => {
-  if (afterSubmit) {
+  if (formSubmitted) {
     const data = getDataFromForm(form, fields)
 
     const errors = validateData(data, fieldsValidationRules)
 
     handleErrors(errors, form)
+  }
+})
+
+const form1 = document.querySelector('#form1')
+
+let form1Submitted = false
+
+form1.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const data = getDataFromForm(form1, fieldsForForm1)
+
+  const errors = validateData(data, fieldsValidationRulesForForm1)
+
+  handleErrors(errors, form1)
+
+  form1Submitted = true
+
+  if (errors.length === 0) {
+    console.log(data)
+  }
+})
+
+form1.addEventListener('input', (event) => {
+  if (form1Submitted) {
+    const data = getDataFromForm(form1, fields)
+
+    const errors = validateData(data, fieldsValidationRulesForForm1)
+
+    handleErrors(errors, form1)
   }
 })
